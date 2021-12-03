404029f06601b22a8de9f20af90be57ad0fa0ea9
Shweta3117 <90396035+Shweta3117@users.noreply.github.com>
2021-12-03 12:03:37 +0530

    Create index.js

---
+++ src/Components/Molecules/Navbar/NavbarToggle/index.js
@@ -0,0 +1,7 @@
+export const NavbarToggle = ({onClick}) => {
+  return (
+    <button className="navbar-toggler" type="button" onClick={onClick}>
+      <span className="navbar-toggler-icon" />
+    </button>
+  )
+}
