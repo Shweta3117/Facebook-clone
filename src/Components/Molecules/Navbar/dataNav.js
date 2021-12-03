e3211029960bc1b188cc4695a904aa67b5c7ab4f
Shweta3117 <90396035+Shweta3117@users.noreply.github.com>
2021-12-03 12:04:18 +0530

    Create dataNav.js

---
+++ src/Components/Molecules/Navbar/dataNav.js
@@ -0,0 +1,34 @@
+export const dataNav = {
+  navigation_1: [
+    {
+      icon: "bxs-home",
+      url: "/",
+      title: "Beranda",
+      class: "nav-link"
+    },
+    {
+      icon: "bxs-user-plus",
+      url: "/tambah-teman",
+      title: "Tambah Teman",
+      class: "nav-link"
+    },
+    {
+      icon: "bxs-video",
+      url: "/tonton-video",
+      title: "Siaran Langsung",
+      class: "nav-link"
+    },
+    {
+      icon: "bxs-store",
+      url: "/market",
+      title: "Market",
+      class: "nav-link"
+    },
+    {
+      icon: "bxs-group",
+      url: "/group",
+      title: "Grup",
+      class: "nav-link hide-md"
+    },
+  ]
+}
