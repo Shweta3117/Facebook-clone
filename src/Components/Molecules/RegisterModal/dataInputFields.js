f7691d3275e18cfabdcc7fcace334f4a43ba27e3
Shweta3117 <90396035+Shweta3117@users.noreply.github.com>
2021-12-03 12:07:15 +0530

    first commit

---
+++ src/Components/Molecules/RegisterModal/dataInputFields.js
@@ -0,0 +1,32 @@
+const dataInputFields = [
+  {
+    id: 'firstName',
+    type: 'text',
+    text: 'Nama depan',
+    col: 'col col-sm-6',
+    uniqueId: 456232,
+  },
+  {
+    id: 'lastName',
+    type: 'text',
+    text: 'Nama belakang',
+    col: 'col col-sm-6',
+    uniqueId: 634233,
+  },
+  {
+    id: 'emailField',
+    type: 'email',
+    text: 'Email',
+    col: 'col-md-4 col-md-12',
+    uniqueId: 523234,
+  },
+  {
+    id: 'passwordField',
+    type: 'password',
+    text: 'Kata Sandi baru',
+    col: 'col-md',
+    uniqueId: 452343,
+  },
+]
+
+export default dataInputFields
