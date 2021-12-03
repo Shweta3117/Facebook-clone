0bcfd920143fdd692f84f30bdf9920fd5fd8a01a
Shweta3117 <90396035+Shweta3117@users.noreply.github.com>
2021-12-03 12:09:01 +0530

    Delete the scss files in components

---
+++ src/Components/Molecules/SearchBar/index.js
@@ -0,0 +1,9 @@
+import InputField from '../../Atom/Input'
+
+const SearchBar = () => {
+  return (
+    <InputField type="text" placeholder="Cari di Facebook" id="searchBar"/>
+  )
+}
+
+export default SearchBar
